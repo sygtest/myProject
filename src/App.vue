2ec1feb859bf2ee638c4802d736dd2a9e7784959
cyg <cjcyg2009@qq.com>
2021-02-06 17:56:26 +0800

    第一个组件

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <img src="./assets/img/tabbar/home.svg" slot="item-icon">
+        <img src="./assets/img/tabbar/home_active.svg" slot="item-icon-active">
+        <div slot="item-name">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category">
+        <img src="./assets/img/tabbar/category.svg" slot="item-icon">
+        <img src="./assets/img/tabbar/category_active.svg" slot="item-icon-active">
+        <div slot="item-name">分类</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart">
+        <img src="./assets/img/tabbar/shopcart.svg" slot="item-icon">
+        <img src="./assets/img/tabbar/shopcart_active.svg" slot="item-icon-active">
+        <div slot="item-name">购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <img src="./assets/img/tabbar/profile.svg" slot="item-icon">
+        <img src="./assets/img/tabbar/profile_active.svg" slot="item-icon-active">
+        <div slot="item-name">我的</div>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from './components/tabbar/TabBar'
+import TabBarItem from './components/tabbar/TabBarItem'
+
+  
+export default {
+  name: 'App',
+  components: {
+   TabBar,
+   TabBarItem
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+
+
+
+</style>
